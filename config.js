827a3a4f1affd86ab3cd60c171df3b89598689d0
MattiaAlba <54856912+MattiaAlba@users.noreply.github.com>
2021-04-18 00:09:04 -0400

    1.0

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports={
+    consumer_key:'4Li1jVpXlH6fdFaHCPvMrJXuI',
+    consumer_secret:'2CLJHzLb28KiOUXN25tdH7XNgP1LH0riaXvi4efEy6iWLp3F9u',
+    access_token:'1383027197179486210-7XKEH6cX84n1xcbscbU0V3QLH8dn3C',
+    access_token_secret:'HtlD33jwIDBodDagGdAFOYkkcIPxHLvwBDnzLJCtZqjUp',
+}
---
+++ index.js
@@ -0,0 +1,26 @@
+const config = require('./config.js');
+const Twitter = require('twit');
+var randomWords = require('random-words');
+
+const fs = require("fs");   //filesystem
+
+var bot = new Twitter(config);
+
+var tweet = {status: 'Understand the concept of ' + randomWords()}
+
+
+if(process.argv[2]){
+    console.log("***Custom message argv 2*** = " + process.argv[2]);
+    bot.post('statuses/update', {status: 'Understand the concept of ' + process.argv[2]}, error);
+}
+
+else{
+    bot.post('statuses/update', tweet,  error);
+}
+
+function error(err, data, res){
+    if (err)
+        console.log("BAD", data)
+    else
+        console.log("GOOD")
+}
