2a90818a9fbc974fd8db33fcfe5dac2970738ce8
mattia.albabb@gmail.com <mattia.albabb@gmail.com>
2021-04-18 01:19:12 -0400

    newest++

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 
 //hello
-const config = require('./config.js');
 const Twitter = require('twit');
 var randomWords = require('random-words');
 
 const fs = require("fs");   //filesystem
 
-var bot = new Twitter(config);
+var bot = new Twitter({
+        consumer_key: CONSUMER_KEY,
+        consumer_secret: CONSUMER_SECRET,
+        access_token: ACCESS_TOKEN,
+        access_token_secret: ACCESS_TOKEN_SECRET,
+    }
+);
 
 var tweet = {status: 'Understand the concept of ' + randomWords()}
 
@@ -15,7 +20,6 @@ if(process.argv[2]){
     console.log("***Custom message argv 2*** = " + process.argv[2]);
     bot.post('statuses/update', {status: 'Understand the concept of ' + process.argv[2]}, error);
 }
-
 else{
     bot.post('statuses/update', tweet,  error);
 }
